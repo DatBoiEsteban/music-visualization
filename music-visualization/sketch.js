2b332f83e6d3239cb883b7e9a4a8ae329680f29d
Esteban Mata <esteban1113001@gmail.com>
2021-09-29 14:39:05 -0600

    button play/pause

--- music-visualization/sketch.js
+++ music-visualization/sketch.js
@@ -1,12 +1,24 @@
 var song;
 var barra_volumen;
 
+var boton_play;
+
 function setup() {
   createCanvas(200, 200);
   barra_volumen = createSlider(0, 1, 0, 0.01);
-  song = loadSound("song.mp3", () => {
+  boton_play = createButton("play");
+  boton_play.mousePressed(togglePlay);
+  song = loadSound("song.mp3");
+}
+
+function togglePlay() {
+  if (!song.isPlaying()) {
     song.play();
-  });
+    boton_play.html("pause");
+  } else {
+    song.pause();
+    boton_play.html("play");
+  }
 }
 
 function draw() {
